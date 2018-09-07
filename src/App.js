5403ecea494f35f82a2a890840c986393b1e6397
peterkahenya <peterkahenyanjoki@gmail.com>
2018-09-07 08:28:46 +0300

    The App more granularised

--- src/App.js
+++ src/App.js
@@ -1,27 +1,45 @@
-import React, { Component } from 'react';
-import './App.css';
-import LogoAndName from './components/LogoAndName';
-import SearchBar from './components/SearchBar';
+import React, { Component } from "react";
+import "./App.css";
+import LogoAndName from "./components/LogoAndName";
+import SearchBar from "./components/SearchBar";
+import ResultsList from "./components/ResultsList";
+
+import Grid from "@material-ui/core/Grid";
+import color from "@material-ui/core/colors/pink";
 
 class App extends Component {
-  constructor(){
-    super();
-    fetch("http://api.kmhfltest.health.go.ke/api/facilities/facilities/",{headers:{
-      header:{
-        "Authorization":'Basic '
-      }
-    }})
-
-  }
   render() {
     return (
       <div className="App">
-     
-      <LogoAndName/>
-      
-      <SearchBar/>
+        <Grid spacing={16}>
+          <LogoAndName />
+          <SearchBar />
+          <ResultsList />
+        </Grid>
       </div>
     );
+  }
+
+  componentWillMount() {
+    fetch(
+      "http://api.kmhfltest.health.go.ke/api/facilities/facilities/?page=1&format=json",
+      {
+        headers: {
+          Authorization: "Bearer ze5uDYpiqxPXZJCxinLahpOdDx9T6T",
+          "Content-Type": "application/json"
+        }
+      }
+    )
+      .then(fetchData => fetchData.json())
+      .then((jsonData.results) => {
+        jsonData.map(facility, () => {
+
+         { <SearchResult name={facility.name} />}
+        });
+      })
+      .catch(error => {
+        console.warn("error", error);
+      });
   }
 }
 
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,26 +1,44 @@
-import React from 'react'
-import Button from '@material-ui/core/Button'
-import { Input } from '@material-ui/core';
+import React from "react";
+import Button from "@material-ui/core/Button";
+import { Input } from "@material-ui/core";
+import Search from "@material-ui/icons/Search";
+import LocationSearching from "@material-ui/icons/LocationSearching";
+import FilterList from "@material-ui/icons/FilterList";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
 
+import Avatar from "@material-ui/core/Avatar";
 
-export default class SearchBar extends React.Component{
-    constructor(){
-        super();
-    }
-    render(){
-        return(
-            <div>
-            <Button>
-            Search Filters
-          </Button>
-      <Input title="Search Name or code"/>
-          <Button>
-            Search
-          </Button>
-          <Button>
-            Map Icon
-          </Button>
-          </div>
-        );
-    }
+export default class SearchBar extends React.Component {
+  render() {
+    return (
+      <Grid
+        container
+        direction="row"
+        justify="center"
+        alignItems="center"
+        xs={24}
+      >
+        <Avatar>
+          <FilterList />
+        </Avatar>
+        <TextField
+          id="full-width"
+          label="Search"
+          InputLabelProps={{
+            shrink: true
+          }}
+          style={{ width: 600 }}
+          placeholder="Search Name,MFL code or location"
+          margin="normal"
+        />
+        <Avatar>
+          <Search />
+        </Avatar>
+        <Avatar>
+          <LocationSearching />
+        </Avatar>
+      </Grid>
+    );
+  }
 }
