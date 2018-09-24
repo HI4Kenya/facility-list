5cc40f011777f426756482fd2dd3f6f9b19a2236
pkiar <kiariepete@gmail.com>
2018-09-24 20:21:15 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// DOM Element to Render our app
+const rootElement = document.querySelector("#root");
+
+const headers ={
+    headers:{
+        'Authorization': `Basic ${btoa('pkiar:Sent1ment21')}`
+}
+}
+
+// DataSet function - View
+
+function OrganisationUnit({organisationunit}){
+    return <div><a href={organisationunit.href}>{organisationunit.name}</a></div>;
+}
+
+// DataSet List
+
+function OrganisationUnitList({organisationunits}){
+    return (
+        <div>
+            <h2>OrganisationUnits</h2>
+            <div>{organisationunits.map((organisationunit) => <OrganisationUnit organisationunit={organisationunit} />)}</div>
+        </div>
+    );
+}
+
+// Pulling organisationUnits via API
+fetch('http://197.136.81.99:8082/test/api/organisationUnits?paging=false&fields=id,name,code,shortName,description', headers
+).then((fetchData) => fetchData.json()).then((jsonData)=>{
+    ReactDOM.render(<OrganisationUnitList organisationunits={jsonData.organisationUnits}/>, rootElement);
+}).catch((error)=>{console.log('Error', error)});
